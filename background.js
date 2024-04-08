e609059efe0ef03c2bb120f2ba596643687254fa
yamaji <haruyuki.program@gmail.com>
2024-04-08 13:16:10 +0900

    👍 バッジに今日の記録数を表示するように

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ import { IndexedDB } from "./IndexedDB/IndexedDB.js";
 import { dbname, dbcolumns } from "./utils/db.config.js";
 
 async function log() {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   console.log(tab.url, tab.title, tab.favIconUrl);
   const db = await IndexedDB.create(dbname, dbcolumns);
   const item = {
@@ -12,8 +12,28 @@ async function log() {
     url: tab.url,
   };
   db.add(item);
+  updateBadge();
+}
+
+async function updateBadge() {
+  const db = await IndexedDB.create(dbname, dbcolumns);
+  const now = new Date();
+  const today = new Date(
+    `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
+  ).getTime();
+  const count = (await db.getAll())
+    .filter((item) => item.time > today)
+    .length.toString();
+  chrome.action.setBadgeText({
+    text: count,
+  });
+  chrome.action.setBadgeBackgroundColor({
+    color: "#B4E9FF",
+  });
 }
 
 chrome.commands.onCommand.addListener(async (command) => {
   if (command === "log") await log();
 });
+
+updateBadge();
